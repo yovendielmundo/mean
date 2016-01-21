b77cd89921edac986550dd55d59792e912c67e88
Luis Osorio <yovendielmundo@gmail.com>
2016-01-21 17:29:45 +0100

    added e2e test for custom menu

--- tests/e2e/smoke/home.spec.js
+++ tests/e2e/smoke/home.spec.js
@@ -1,6 +1,28 @@
 describe('Smoke test home page', function(){
-  it('title should contain Souqalmal Test', function(){
+
+  beforeEach(function() {
     browser.get('/');
+  });
+
+  it('title should contain Souqalmal Test', function(){
     expect(browser.getTitle()).toMatch(/.*Souqalmal test.*/);
   });
+
+  it('should have a list of titles in the menu', function() {
+    var menuNameItem = element.all(by.repeater('item in hdrctr.menus.testThemeMain').column('item.title'));
+
+    function getNames() {
+      return menuNameItem.map(function(elm) {
+        return elm.getText();
+      });
+    }
+
+    expect(menuNameItem.count()).toBe(1);
+
+    expect(getNames()).toEqual([
+      'Page one'
+    ]);
+
+  });
+
 });
